aba5438044b4bf72da7d1ef0e844195268ce750d
Bhavik Pahwa <bhavikpahwa2007@gmail.com>
2022-06-18 19:26:18 +0530

    neew git

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='nav'>
+            <div id='left'>
+                <a className='leftbtn btn'>Home</a>
+            </div>
+            <div id='right'>
+                <a className='rightbtn btn'>Buy Now</a>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
